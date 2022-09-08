89213766f535f5fd0452825d4552123163924673
unknown <jason.rettinger@gmail.com>
2022-09-08 17:44:26 -0400

    added additional questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,22 +4,38 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        name: 'repo',
+        name: 'project',
         type: 'input',
-        message: 'What is the name of your GitHub repository?'
+        message: 'What is the name of your project?'
+    }, 
+    {
+        name: 'username',
+        type: 'input',
+        message: 'What is your GitHub username?'
     },
-
+    {
+        name: "url",
+        type: "input",
+        message: "What is the URL to your GitHub repository?"
+      },
     {
         name: 'email',
         type: 'input',
         message: 'What is your email address?'
     },    
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Please write a short description of your project.'
+    },    
 
     {
-        name: 'project',
-        type: 'input',
-        message: 'What is the name of your project?'
-    }, 
+        name: "license",
+        type: "checkbox",
+        message: "What kind of license does your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+      },
+
     
 ];
 
